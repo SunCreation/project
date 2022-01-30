dd3b4e1456b7d716547096a8f2cd137e7c7e674a
SunCreation <cake0103@nate.com>
2022-01-30 11:24:37 +0000

    hchang

--- test/2test.py
+++ test/2test.py
@@ -1,19 +1,110 @@
+import os
+from os import path
+import transformers
+import torch
+import numpy
+# import wandb
+from transformers import (
+    AutoTokenizer, 
+    GPT2LMHeadModel,
+    TrainingArguments,
+    Trainer,
+)
 from datasets import load_dataset
+import pandas as pd
+from datasets import load_metric
+import numpy as np
 
-raw_sets = load_dataset("imdb")
-print(raw_sets["train"]['text'][0],raw_sets["train"]['label'][0])
-# from transformers import AutoTokenizer
 
-# tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+def prepare_train_features(examples):
+    tokenized_examples = tokenizer(
+        text=examples['problem'],
+        text_pair=examples['code'],
+        padding='max_length',
+        max_length=216
+    )
+    tokenized_examples["labels"] = tokenized_examples["input_ids"].copy()
+    return tokenized_examples
 
-# inputs = tokenizer(sentences, padding="max_length", truncation=True)
+metric = load_metric("accuracy")
 
-# def tokenize_function(examples):
-#     return tokenizer(examples["text"], padding="max_length", truncation=True)
+def compute_metrics(eval_pred):
+    logits, labels, dasdas = eval_pred
+    print(logits, labels)
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
 
-# tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+# wandb.init(project="kogpt2-pretrained-baseline", entity="math-solver")
 
-# small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000)) 
-# small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000)) 
-# full_train_dataset = tokenized_datasets["train"]
-# full_eval_dataset = tokenized_datasets["test"]
+tokenizer = AutoTokenizer.from_pretrained('skt/kogpt2-base-v2', bos_token='</s>', sep_token='<sep>', eos_token='</s>', pad_token='<pad>')
+model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+
+dataset = load_dataset('csv', data_files='../../CloudData/math/data/train.csv', split='train')
+
+dictdataset = dataset.train_test_split(0.1)
+
+tokenizer.decode(prepare_train_features(dictdataset['train'][0])['input_ids'])
+
+tokenized_datasets = dictdataset.map(prepare_train_features, batched=True, remove_columns=dictdataset["train"].column_names)
+
+args = TrainingArguments(
+    output_dir='kogpt-finetune',
+    overwrite_output_dir = True,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=1,
+    num_train_epochs = 1,
+    logging_strategy='epoch',
+    save_strategy = 'epoch',
+    evaluation_strategy = 'epoch',
+    load_best_model_at_end = True,
+    # report_to="wandb"
+)
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=tokenized_datasets['train'],
+    eval_dataset=tokenized_datasets['test'],
+    compute_metrics=compute_metrics
+    # data_collator=data_collator,
+)
+
+output = trainer.train()
+
+print(help(trainer.save_model))
+
+# answer = input("저장 고?")
+# if answer=="N": exit()
+
+trainer.save_model('test-kogpt-trained-hchang')
+
+device = torch.device('cpu')
+
+model = GPT2LMHeadModel.from_pretrained('test-kogpt-trained-hchang').to(device)
+
+def solve_problem(problem):
+    input_ids = tokenizer(problem,return_tensors='pt')['input_ids']
+    output = model.generate(input_ids, max_length = 216)
+    sentence = tokenizer.decode(output[0].numpy().tolist())
+    sentence = sentence.split('<pad>')[0]
+    sentence = sentence.lstrip(problem)
+    sentence = sentence.lstrip(' ')
+    print('=====')
+    print(f'{sentence}')
+    print('실행결과:')
+    try:
+        exec(sentence)
+    except:
+        print('error')
+    print("")
+
+test = pd.read_csv('KMWP/data/test.csv')
+
+import random
+
+for _ in range(5):
+    i = random.randint(0, 281)
+    p = test.iloc[i]['problem']
+    print(f'{p}')
+    solve_problem(p)
+
--- test/test2.py
+++ test/test2.py
@@ -4,14 +4,16 @@ from transformers import pipeline
 import tensorflow as tf
 from transformers import AutoTokenizer
 from transformers import TFGPT2LMHeadModel
+
 tokenizer = AutoTokenizer.from_pretrained('skt/kogpt2-base-v2', bos_token='</s>', eos_token='</s>', pad_token='<pad>')
 model = TFGPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2', from_pt=True)
+from core import LearningRateScheduler
 
 
-inputs = tokenizer("예진님 짱짱", return_tensors="tf")
+inputs = tokenizer("예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱예진님 짱짱", return_tensors="tf")
 sample = tokenizer.tokenize("예진님 짱짱")
 outputs = model(inputs)
-print(sample)
+print(model.config)
 
 
 # print(tokenizer.cls_token)
@@ -23,14 +25,13 @@ data_path = "/workspace/CloudData/math/data/train.csv"
 train_data = pd.read_csv(data_path)
 print(train_data.head(3))
 
-BATCH_SIZE = 8
+BATCH_SIZE = 10
 
 def get_math_data():
     for _class, problem, code, ans in zip(train_data["class"][:-100], train_data.problem.to_list()[:-100], train_data.code.to_list()[:-100], train_data["answer"][:-100]):
         bos_token = [tokenizer.bos_token_id]
         eos_token = [tokenizer.eos_token_id]
         sent = tokenizer.encode(problem+'<sys>' + code)
-        print(bos_token + sent + eos_token)
         yield bos_token + sent + eos_token
 
 dataset = tf.data.Dataset.from_generator(get_math_data, output_types=tf.int32)
@@ -43,8 +44,8 @@ for batch in dataset:
 
 print(tokenizer.decode(batch[0]))
 
-
-adam = tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08)
+learning_rate=LearningRateScheduler(768)
+adam = tf.keras.optimizers.Adam(learning_rate, epsilon=1e-08)
 
 steps = len(train_data) // BATCH_SIZE + 1
 # print(steps)
@@ -53,8 +54,8 @@ print(batch,type(batch))
 
 _len = len(train_data)-100
 
-answer = input("Enter를 쳐주세요.(멈추려면 N)")
-if answer == "N":exit()
+# answer = input("Enter를 쳐주세요.(멈추려면 N)")
+# if answer == "N":exit()
 
 # --------------------------
 # def get_math_data():
@@ -65,32 +66,63 @@ if answer == "N":exit()
 #         yield bos_token + sent + eos_token
 
 # model.train()
-# mnli
+
+
+
+def solve_problem(problem):
+    sent = problem + '<sys>'
+    input_ids = [tokenizer.bos_token_id] + tokenizer.encode(sent)
+    input_ids = tf.convert_to_tensor([input_ids])
+    output = model.generate(input_ids, max_length = 50, do_sample=True, top_k = 20)
+    sentence = tokenizer.decode(output[0].numpy().tolist())
+    # _class = sentence.split('<usr>')[1].replace('</s>', '').split('<sys>')[0].strip()
+    code = sentence.split('<sys>')[1].replace('</s>', '').strip()
+    # answer = sentence.split('<sys>')[2].replace('</s>', '').strip()
+    return code #answer
+
+test_path = "/workspace/CloudData/math/data/test.csv"
+
+test_data = pd.read_csv(test_path)
 
 from tqdm import tqdm
-EPOCHS = int(input("학습시작, 몇에폭? : "))
+from IPython import display
 
-for epoch in range(EPOCHS):
-    epoch_loss = 0
+fit = True
+display.clear_output(wait=True)
+while fit:
+    EPOCHS = int(input("학습시작, 몇에폭? : "))
 
-    idx_list = list(range(0, _len, BATCH_SIZE))[:-3]
-    # random.shuffle(idx_list)
-    t = tqdm(idx_list)
-    nowdata = dataset.__iter__()
+    for epoch in range(EPOCHS):
+        epoch_loss = 0
 
-    for batch,_ in enumerate(t):
-        with tf.GradientTape() as tape:
-            now = next(nowdata)
-            result = model(now, labels = now)
-            loss = result[0]
-            # print(help(result))
-            batch_loss = tf.reduce_mean(loss)
-          
-        grads = tape.gradient(batch_loss, model.trainable_variables)
-        adam.apply_gradients(zip(grads, model.trainable_variables))
-        epoch_loss += batch_loss
-        t.set_description_str('Epoch %2d' % (epoch + 1))    
-        t.set_postfix_str('Loss %.4f' % (epoch_loss / (batch + 1)))
+        idx_list = list(range(0, _len, BATCH_SIZE))[:-1]
+        # random.shuffle(idx_list)
+        t = tqdm(idx_list)
+        nowdata = dataset.__iter__()
+
+        for batch,_ in enumerate(t):
+            with tf.GradientTape() as tape:
+                now = next(nowdata)
+                result = model(now, labels = now)
+                loss = result[0]
+                # print(help(result))
+                batch_loss = tf.reduce_mean(loss)
+            
+            grads = tape.gradient(batch_loss, model.trainable_variables)
+            adam.apply_gradients(zip(grads, model.trainable_variables))
+            epoch_loss += batch_loss
+            t.set_description_str('Epoch %2d' % (epoch + 1))    
+            t.set_postfix_str('Loss %.4f' % (epoch_loss / (batch + 1)))
+
+    for i in test_data["problem"][:5]:
+        print(i)
+        print("=====================")
+        print(solve_problem(i))
+        print("")
+    answer = input("학습을 마칠까요?(아니면 N): ")
+    if answer!="N": fit=False
+
+
 
         # for i in 
 
@@ -115,27 +147,10 @@ for epoch in range(EPOCHS):
 
 
 
-answer = input("학습이 끝났습니다.")
 
-test_path = "/workspace/CloudData/math/data/test.csv"
-
-test_data = pd.read_csv(test_path)
-print(test_data.head())
+# print(test_data.head())
 
 
-def solve_problem(problem):
-    sent = problem + '<sys>'
-    input_ids = [tokenizer.bos_token_id] + tokenizer.encode(sent)
-    input_ids = tf.convert_to_tensor([input_ids])
-    output = model.generate(input_ids, max_length = 50, do_sample=True, top_k = 20)
-    sentence = tokenizer.decode(output[0].numpy().tolist())
-    # _class = sentence.split('<usr>')[1].replace('</s>', '').split('<sys>')[0].strip()
-    code = sentence.split('<sys>')[1].replace('</s>', '').strip()
-    # answer = sentence.split('<sys>')[2].replace('</s>', '').strip()
-    return code #answer
-
-for i in test_data["problem"][:5]:
-    print(solve_problem(i))
 
 answer = input("저장하시겠습니까?(아니면 N):")
 # tf.Checkpoint
